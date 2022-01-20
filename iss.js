9ed631f33f7655aef0631bcd986d70e28dab1057
Vivian Zhang <vvynz12@gmail.com>
2022-01-20 23:00:19 +0000

    added a 2nd if statement to check if response state codes !== 200, which would assume a server error

--- iss.js
+++ iss.js
@@ -16,9 +16,16 @@ const fetchMyIP = function (callback) {
 
   request(url, (error, response, body) => {
     const IP = JSON.parse(body).ip;
-
+    // error can be set if invalid domain, user is offline, etc.
     if (error) {
       callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${respon.statusCode} when fetching IP. Response ${body}.`;
+      callback(error(msg), null);
+      return;
     }
 
     callback(null, IP);
